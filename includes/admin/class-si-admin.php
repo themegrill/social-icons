f6ded011b96d25c1614dfebf38e073931d6756a3
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-05-18 15:06:25 +0545

    Added social icons tinMCE shortcode plugin

--- includes/admin/class-si-admin.php
+++ includes/admin/class-si-admin.php
@@ -33,8 +33,9 @@ class SI_Admin {
 	public function includes() {
 		include_once( 'functions-si-admin.php' );
 		include_once( 'functions-si-meta-box.php' );
-		include_once( 'class-si-admin-assets.php' );
 		include_once( 'class-si-admin-post-types.php' );
+		include_once( 'class-si-admin-assets.php' );
+		include_once( 'class-si-admin-tinymce.php' );
 	}
 
 	/**
