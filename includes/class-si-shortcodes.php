a3485ecd837580ad5df8bd46d4e0ed96a09f331a
Bishal Napit <mebishalnapit@gmail.com>
2021-01-11 11:20:13 +0545

    Tweak - Strict comparision

--- includes/class-si-shortcodes.php
+++ includes/class-si-shortcodes.php
@@ -116,10 +116,10 @@ class SI_Shortcodes {
 				$background = 'text' !== $color_type ? 'background-color: ' . $icon_color : 'color: ' . $icon_color; ?>
 
 				<li class="social-icons-list-item">
-					<a href="<?php echo esc_url( $field['url'] ); ?>" <?php echo ( 'yes' == $group_data['open_new_tab'] ? 'target="_blank"' : '' ); ?> <?php echo ( 'yes' == $group_data['add_nofollow'] ? 'rel="nofollow"' : '' ); ?> class="social-icon">
+					<a href="<?php echo esc_url( $field['url'] ); ?>" <?php echo ( 'yes' === $group_data['open_new_tab'] ? 'target="_blank"' : '' ); ?> <?php echo ( 'yes' === $group_data['add_nofollow'] ? 'rel="nofollow"' : '' ); ?> class="social-icon">
 						<span class="socicon socicon-<?php echo esc_attr( $class ); ?>" style="padding: <?php echo esc_attr( $icon_padding ); ?>px; font-size: <?php echo esc_attr( $icon_font_size ); ?>px; <?php echo esc_attr( $background ); ?>"></span>
 
-						<?php if ( 'yes' == $group_data['manage_label'] ) : ?>
+						<?php if ( 'yes' === $group_data['manage_label'] ) : ?>
 							<span class="social-icons-list-label"><?php echo esc_html( $field['label'] ); ?></span>
 						<?php endif; ?>
 					</a>
